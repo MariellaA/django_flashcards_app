7fa0f17139c2ecff80c64cdb113770f746fec074
Mariela <mariela.angelova99@gmail.com>
2023-10-13 01:47:50 +0300

    Add move functionality when card is guessed or not

--- cards/models.py
+++ cards/models.py
@@ -14,5 +14,18 @@ class Card(models.Model):  # The Card model defines that the database should con
     date_created = models.DateTimeField(auto_now_add=True)  # With the date_created field, the newest cards can be shown
     # first in the card overview
 
+    def move(self, solved):
+        new_box = self.box + 1 if solved else BOXES[0]  # evaluating whether the card should be moved forward into the
+        # next box or back to the first box.
+
+        # Note that new_box could even be 6 if the answer to a card that resided in your fifth box is correct.
+        # That’s why the new self.box value is saved only when new_box is a number from 1 to 5.
+        # If the answer for a card in the fifth box is guessed, then the card stays in the fifth box and doesn’t move.
+        if new_box in BOXES:
+            self.box = new_box
+            self.save()
+
+        return self
+
     def __str__(self):  # Used to control the string representation of the Card objects
         return self.question
